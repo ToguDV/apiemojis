98e0a8c54224838353448ae3108b5fe2cee75db3
Togu <timeofgetup@gmail.com>
2024-03-09 08:00:53 -0500

    Add trivia get

---
+++ apiemojis/Controllers/TriviaController.cs
@@ -0,0 +1,24 @@
+﻿using apiemojis.Data;
+using apiemojis.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace apiemojis.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TriviaController : ControllerBase
+    {
+        private readonly DataContext _context;
+        public TriviaController(DataContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Trivia
+        [HttpGet]
+        public ActionResult<IEnumerable<Trivia>> GetTrivias()
+        {
+            return _context.Trivias.ToList();
+        }
+    }
+}
