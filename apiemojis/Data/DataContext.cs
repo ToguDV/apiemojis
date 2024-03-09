d453cbde7e817af00355d1ae700fe2264f3f50a7
Togu <timeofgetup@gmail.com>
2024-03-08 23:35:47 -0500

    Add project files.

---
+++ apiemojis/Data/DataContext.cs
@@ -0,0 +1,26 @@
+﻿using apiemojis.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace apiemojis.Data
+{
+    public partial class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+
+        public virtual DbSet<Trivia> Trivias { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Trivia>(entity =>
+            {
+                entity.HasKey(k => k.TriviaId);
+            });
+            OnModelCreatingPartial(modelBuilder);
+        }
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+
+
+    }
+}
---
+++ apiemojis/Models/Trivia.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace apiemojis.Models
+{
+    public class Trivia
+    {
+        [Key]
+        public int TriviaId { get; set; }
+        [Required]
+        [MaxLength(100)]
+        public string Title { get; set; }
+        [Required]
+        [MaxLength(100)]
+        public string Description { get; set; }
+        [Required]
+        [MaxLength(200)]
+        public string Emojis { get; set; }
+    }
+}
